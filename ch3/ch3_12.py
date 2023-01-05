0d250d74dc97aac8e96566998901145de0e678dd
유주성 <114473861+Juseong-Yu@users.noreply.github.com>
2023-01-05 22:53:57 +0900

    Update ch3_12.py

--- ch3/ch3_12.py
+++ ch3/ch3_12.py
@@ -1,6 +1,37 @@
 year = int(input('연도를 입력하시오: '))
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    print('윤년입니다.')
 
-else:
-    print('윤년이 아닙니다.')
+if year % 12 == 0:
+    print('원숭이띠')
+
+elif year % 12 == 1:
+    print('닭띠')
+
+elif year % 12 == 2:
+    print('개띠')
+
+elif year % 12 == 3:
+    print('돼지띠')
+
+elif year % 12 == 4:
+    print('쥐띠')
+
+elif year % 12 == 5:
+    print('소띠')
+
+elif year % 12 == 6:
+    print('호랑이띠')
+
+elif year % 12 == 7:
+    print('토끼띠')
+
+elif year % 12 == 8:
+    print('용띠')
+
+elif year % 12 == 9:
+    print('뱀띠')
+
+elif year % 12 == 10:
+    print('말띠')
+
+elif year % 12 == 11:
+    print('양띠')
